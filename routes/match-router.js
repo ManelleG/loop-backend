68f4121f45e87632845c8eac0675a9dd42ba0748
Marie Gillier <magillier@hotmail.fr>
2018-07-25 12:36:52 +0200

    find a way to access results from GMap

--- routes/match-router.js
+++ routes/match-router.js
@@ -10,40 +10,77 @@ var googleMapsClient = require('@google/maps').createClient({
   Promise: Promise
 });
 
+var db = [{
+  city1: "habas, France",
+  city2: "route de pau, orthez, France"
+}, {
+  city1: "amou, France",
+  city2: "mugron, France"
+}, {
+  city1: "route de cazalis, momuy, France",
+  city2: "amou, France"
+}, {
+  city1: "sallespisse, France",
+  city2: "hagetmau, France"
+}, {
+  city1: "peyhorade, France",
+  city2: "habas, France"
+}, {
+  city1: "dax, France",
+  city2: "route de galin, tilh, France"
+}
+];
 
 
-function calcDrivDur(){
-
+function calcUserDur(){
   googleMapsClient.directions(
-    //1er argument de la fonction directions: la requete trajet conducteur
     {	
       origin: "28 Av. des Champs-Élysées, 75008 Paris, France",
       destination: "20 Rue de Brigode, 59000 Lille, France",
       mode: "driving"
-    }, 
-
-    //2e arg de la fct directions: call back function qui donne le trajet conducteur 
-    function(err, response) {
-      console.log(err);
-      console.log(response);
-       //durconducteur = response.routes[0].legs[0].duration.value
-
-        if (!err) { 
-        callback(response.json.results.routes[0].legs[0].duration.value);
-        //console.log(response.routes[0].legs[0].duration.value)
-        };
     }
-  );
+  ).asPromise()
+  .then((response) => {
+    let userDur = response.json.routes[0].legs[0].duration.value
+    console.log(userDur)
+    console.log(response);
+    return userDur;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 };
   
- var input = {
-  origin: "28 Av. des Champs-Élysées, 75008 Paris, France",
-  destination: "20 Rue de Brigode, 59000 Lille, France",
-  mode: "driving"
- }
+//Calculates the best match when the trip that has been submitted is a driver trip
+function calcBestMatchDriv(){
 
+//   googleMapsClient.directions(
+  
+//   for (k = 0, lengg = db.length; k < lengg; k++) {
+//     {	
+//       origin: "28 Av. des Champs-Élysées, 75008 Paris, France",
+//       destination: "20 Rue de Brigode, 59000 Lille, France",
+//       waypoints: [{
+//         location: db[k].city1,
+//         stopover: true
+//       }, {
+//         location: db[k].city2,
+//         stopover: true
+//       }
+//     ],
+//       mode: "driving"
+//     }
+//   ).asPromise()
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-calcDrivDur();
+// })
+
+calcUserDur();
 
 
 
