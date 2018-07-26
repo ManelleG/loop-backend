66adf48ecb072d7a7bc2e4305901175a9436aba3
Manelle_G;` <manelle.gueriouz@gmail.com>
2018-07-26 16:58:51 +0200

    merging issues

--- models/trip-model.js
+++ models/trip-model.js
@@ -2,70 +2,75 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const tripSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+const tripSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    startLocation: {
+      type: { type: String, default: "Point" },
+      string: { type: String },
+      coordinates: [{ type: Number }]
+    },
+    endLocation: {
+      type: { type: String, default: "Point" },
+      string: { type: String },
+      coordinates: [{ type: Number }]
+    },
+    departDateAndTime: { type: Date, required: true },
+    // departTime: { type: String, required: true},
+    comment: { type: String },
+    numberOfSeats: { type: Number }
   },
-  startLocation: {
-    type: { type: String, default: "Point"},
-    string: { type: String },
-    coordinates: [
-     {type: Number}
-    ]
-  },
-  endLocation: {
-    type: { type: String, default: "Point" },
-    string: { type: String },
-    coordinates: [
-     {type: Number}
-    ]
-  },
-  departDateAndTime: { type: Date, required: true},
-  // departTime: { type: String, required: true},
-  comment: { type: String },
-  numberOfSeats: { type: Number }
-}, {
-  timestamps: true
-})
+  {
+    timestamps: true
+  }
+);
 
-tripSchema.index({startLocation: '2dsphere'})
-tripSchema.index({endLocation: '2dsphere'})
+tripSchema.index({ startLocation: "2dsphere" });
+tripSchema.index({ endLocation: "2dsphere" });
 
+tripSchema.statics.findNear = function findNear(startCoor, endCoor, role) {
+  const startQuery = this.find({
+    startLocation: {
+      $near: {
+        $maxDistance: 50000,
+        $geometry: { type: "Point", coordinates: startCoor }
+      }
+    }
+  }).populate({ path: "user" });
 
+  const endQuery = this.find({
+    endLocation: {
+      $near: {
+        $maxDistance: 50000,
+        $geometry: { type: "Point", coordinates: endCoor }
+      }
+    }
+  }).populate({ path: "user" });
 
-
-tripSchema.statics.findNear = function findNear(startCoor, endCoor, role){
-  const startQuery =
-  this.find({ startLocation: { $near: { $maxDistance: 50000, $geometry: { type: "Point", coordinates: startCoor}}}
-  }).populate({path: 'user'});
-
-  const endQuery =
-  this.find({ endLocation: { $near: { $maxDistance: 50000, $geometry: { type: "Point", coordinates: endCoor}}}
-  }).populate({path: 'user'});
-
-  return Promise.all([startQuery, endQuery])
-  .then((results)=>{
+  return Promise.all([startQuery, endQuery]).then(results => {
     const [startResults, endResults] = results;
 
     let intersectionResults = [];
 
-    startResults.forEach((docA) => {
-      endResults.forEach((docB) => {
-        if (docA._id.toString() === docB._id.toString()){
-          intersectionResults.push(docA)
+    startResults.forEach(docA => {
+      endResults.forEach(docB => {
+        if (docA._id.toString() === docB._id.toString()) {
+          intersectionResults.push(docA);
         }
-      })
-    })
+      });
+    });
 
-    intersectionResults =
-      intersectionResults.filter( tripDoc => (tripDoc.user.isDriver !== role));
+    intersectionResults = intersectionResults.filter(
+      tripDoc => tripDoc.user.isDriver !== role
+    );
 
-    return intersectionResults
-  })
-
-}
+    return intersectionResults;
+  });
+};
 
 const Trip = mongoose.model("Trip", tripSchema);
 
